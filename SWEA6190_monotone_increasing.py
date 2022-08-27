84e563aa9a26497465ad828a6f0c16fe44b770fb
조은비 <mathcom128@gmail.com>
2022-08-27 22:04:15 +0900

    2

--- SWEA6190_monotone_increasing.py
+++ SWEA6190_monotone_increasing.py
@@ -3,21 +3,19 @@ T = int(input())
 for t in range(1,T+1):
     N = int(input())
     num = list(map(int, input().split()))
-    num_list = []
+    mono = []  # 단조 증가하는 수를 담을 리스트
     for i in range(N):
         for j in range(N):
             if i != j:                      # 같은 수끼리 곱해져서 단조증가되는 경우 제외
-                num_list.append(num[i]*num[j])
-    mono = []   # 단조 증가하는 수를 담을 리스트
-    for ent in num_list:
-        mono.append(ent)
-        while ent:
-            r = ent%10     # 나머지와
-            ent = ent//10  # 몫의 크기를 비교
-            ent0 = ent%10
-            if ent0 > r:
-                mono.pop()
-                break
+                ent = num[i]*num[j]
+                mono.append(ent)
+                while ent:
+                    r = ent%10     # 나머지와
+                    ent = ent//10  # 몫의 크기를 비교
+                    ent0 = ent%10
+                    if ent0 > r:
+                        mono.pop()
+                        break
     if len(mono):   # 공집합이 아닌 경우만 실행
         max_ent = mono[0]
         for ent in mono:

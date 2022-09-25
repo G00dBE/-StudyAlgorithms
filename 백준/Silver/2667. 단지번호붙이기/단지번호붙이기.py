c890d992e01faf9eb1ad98107d3ccc220ffb8120
G00dBE <109403854+G00dBE@users.noreply.github.com>
2022-09-25 15:53:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,20 +1,15 @@
 def dfs(i,j):
     global cnt
-    while True:
+    while len(stack):
+        a, b = stack.pop()
         for di, dj in ((-1,0),(0,1),(1,0),(0,-1)):
-            ni = i + di
-            nj = j + dj
+            ni = a + di
+            nj = b + dj
             if 0 <= ni < N and 0 <= nj <N:
                 if arr[ni][nj]:
                     cnt += 1
                     arr[ni][nj] = 0
                     stack.append((ni, nj))
-        else:
-            if len(stack):
-                a, b = stack.pop()
-                dfs(a, b)
-            else:
-                break
 
 N = int(input())
 arr = [list(map(int,input())) for _ in range(N)]
@@ -36,3 +31,4 @@ ans.sort()
 print(len(ans))
 for i in range(len(ans)):
     print(ans[i])
+
